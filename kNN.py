e45d1a6ff43fe4b856039a3343a0c3c6222c8416
Khadidja CHIKH <39216434+KhadidjaCroft@users.noreply.github.com>
2021-03-19 20:40:53 +0100

    Update kNN.py

--- kNN.py
+++ kNN.py
@@ -7,10 +7,36 @@ def kNN(element,k):
     nearest=set()   
     for d in data:
       if d!=instance:
-        dist=distance(d,i) # using a distance measure (ex : Manhattan)
+        dist=distance(d,i) # ex Manhattan
         nearest.add((dist,d))
-    nearest=list(nearest)
+    #nearest=list(nearest)
     nearest=[nearest[neighbour][1] for neighbour in range(k)]
     #now for the estimation, which is not always required
-    estimation= sum(neighbour[output_ind] for neighbour in nearest)
+    estimation= sum(neighbour[output_ind] for neighbour in nearest)/len(nearest)
     return nearest,estimation
+
+*************using sklearn: example **************
+import random
+from sklearn.neighbors import KNeighborsClassifier
+
+global output_ind
+global data
+global output
+output_ind= the index of the output
+<intialize the data>
+output=[x[output_ind] for x in data]
+data=[x[:-1] for x in data]
+
+#training
+classifier = KNeighborsClassifier(n_neighbors=5)
+classifier.fit(data,output)
+
+#prediction
+print(classifier.predict(random.choice(data)))
+print(classifier.predict_proba(random.choice(data)))
+
+#neighbours
+from sklearn.neighbors import NearestNeighbors
+neighbourhood=NearestNeighbors(n_neighbors=5)
+neighbourhood.fit(data)
+print(neighbourhood.kneighbors(random.choice(data)))
