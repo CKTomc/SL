922c3c4f67be64b19f5c9cf71ac992f176336d47
Khadidja CHIKH <39216434+KhadidjaCroft@users.noreply.github.com>
2021-03-22 01:04:36 +0100

    Update kNN.py

--- kNN.py
+++ kNN.py
@@ -17,7 +17,8 @@ def kNN(element,k):
 
 *************using sklearn: example **************
 import random
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier #from sklearn.neighbors import KNeighborsRegressor
+
 
 global output_ind
 global data
@@ -27,16 +28,19 @@ output_ind= the index of the output
 output=[x[output_ind] for x in data]
 data=[x[:-1] for x in data]
 
+"""Classification"""
 #training
-classifier = KNeighborsClassifier(n_neighbors=5)
+classifier = KNeighborsClassifier(n_neighbors=5) #regressor=KNeighborsRegressor(n_neighbors=5)
 classifier.fit(data,output)
 
 #prediction
 print(classifier.predict(random.choice(data)))
 print(classifier.predict_proba(random.choice(data)))
 
-#neighbours
+"""Neighbours"""
 from sklearn.neighbors import NearestNeighbors
 neighbourhood=NearestNeighbors(n_neighbors=5)
 neighbourhood.fit(data)
 print(neighbourhood.kneighbors(random.choice(data))) # returns distances with neighbours inds
+#to get the weighted graph
+neighbourhood.kneighbors_graph(data)
