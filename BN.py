0d53b2e4203d31c9595a6a44bf147ea7189bd785
Khadidja C <39216434+KhadidjaCroft@users.noreply.github.com>
2021-03-25 13:37:35 +0100

    Update BN.py

--- BN.py
+++ BN.py
@@ -1,1 +1,19 @@
+from sklearn.tree import DecisionTreeClassifier
+import random
+global output_ind
+global data
+global output
+global test
 
+output_ind= the index of the output
+<intialize the data>
+output=[x[output_ind] for x in data]
+data=[x[:-1] for x in data]
+<initialize test data>
+
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+classifier = gnb.fit(data,output)
+testpoint=random.choice(test)
+prediction=classifier.predict(testpoint)
+print(testpoint,prediction)
