29140283cd6da097c58f17c33ac54922c67f9218
Khad C. B <39216434+KhadidjaCroft@users.noreply.github.com>
2021-04-23 05:23:37 +0100

    Update RF.py

--- RF.py
+++ RF.py
@@ -1,7 +1,16 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_classification
-X, y = make_classification(n_samples=1000, n_features=4,  n_informative=2, n_redundant=0,
-                            random_state=0, shuffle=False)
-clf = RandomForestClassifier(max_depth=2, random_state=0)
-clf.fit(X, y)
-print(clf.predict([[0, 0, 0, 0]]))
+import random
+global output_ind
+global data
+global output
+output_ind= the index of the output
+<intialize the data>
+output=[x[output_ind] for x in data]
+data=[x[:-1] for x in data]
+<initialize test data>
+
+classifier = RandomForestClassifier(max_depth=6, random_state=0)
+classifier=classifier.fit(data,output)
+testpoint=random.choice(test)
+prediction=classifier.predict(testpoint)
+print(testpoint,prediction)
